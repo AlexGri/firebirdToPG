e422d9cc849acd5d01133658dbf1bffeb1a4b0b8
alexgri <grishin-alexander@yandex.ru>
2015-03-04 15:41:32 +0400

    получаем метаданные с файрбёрда в виде строки.

---
+++ src/main/scala/org/comsoft/TableMetadataExtractor.scala
@@ -0,0 +1,24 @@
+package org.comsoft
+
+import com.typesafe.config.Config
+
+import sys.process._
+
+/**
+ * Created by alexgri on 04.03.15.
+ */
+class TableMetadataExtractor(pathToIsql:String, fbUser:String, fbPassword:String, dbName:String) {
+
+  def cmdString = s"$pathToIsql -x -u $fbUser -p $fbPassword -ch WIN1251 $dbName"
+  def metadata:String = cmdString !!
+
+}
+
+object TableMetadataExtractor {
+  def apply(config:Config) = {
+    new TableMetadataExtractor(config.getString("db.default.isql"),
+      config.getString("db.default.user"),
+      config.getString("db.default.password"),
+      config.getString("database.name"))
+  }
+}
---
+++ src/test/scala/org/comsoft/MetadataSpec.scala
@@ -0,0 +1,23 @@
+package org.comsoft
+
+import akka.actor.ActorSystem
+import akka.testkit.{ImplicitSender, TestKit}
+import org.scalatest.Matchers
+
+/**
+ * Created by alexgri on 04.03.15.
+ */
+class MetadataSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender with TestBase with Matchers {
+
+  behavior of "Metadata"
+
+  def this() = this(ActorSystem("ExportSystem"))
+
+  it should "be retrieved from db" in {
+    val extractor = TableMetadataExtractor(_system.settings.config)
+    println(extractor.cmdString)
+    val meta = extractor.metadata
+    println(meta)
+    meta.isEmpty shouldBe false
+  }
+}
